f3d53a2f733d953540316b4324bee36c8444903b
Erin Kelley <erinmkelley7@gmail.com>
2017-02-24 09:52:28 -0500

    Added in bottom nav bar to start workouts activity

--- app/src/main/java/com/excy/excy/activities/WorkoutsActivity.java
+++ app/src/main/java/com/excy/excy/activities/WorkoutsActivity.java
@@ -1,7 +1,9 @@
 package com.excy.excy.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.excy.excy.R;
 
@@ -11,5 +13,13 @@ public class WorkoutsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workouts);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottomNavigationView);
+
+        // Need to manually perform a click action on the icon that was clicked in previous activity
+        // or else bottom nav bar will reset actively selected icon upon inflation of this layout
+        View view = bottomNavigationView.findViewById(R.id.action_workouts);
+        view.performClick();
     }
 }
