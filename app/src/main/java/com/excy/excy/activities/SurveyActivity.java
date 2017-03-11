b4533fec08cc1a429d0bfffc5137f85e9f12f118
Erin Kelley <erinmkelley7@gmail.com>
2017-03-10 21:02:01 -0500

    initial layout commit

---
+++ app/src/main/java/com/excy/excy/activities/SurveyActivity.java
@@ -0,0 +1,15 @@
+package com.excy.excy.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.excy.excy.R;
+
+public class SurveyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey);
+    }
+}
