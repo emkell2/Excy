2b9bc26fa1454a4e6d2c1371a1ec65b4c96c25ea
Erin Kelley <erinmkelley7@gmail.com>
2017-03-23 13:23:12 -0400

    Added audio files and combined switch statements for efficiency

--- app/src/main/java/com/excy/excy/activities/WorkoutActivity.java
+++ app/src/main/java/com/excy/excy/activities/WorkoutActivity.java
@@ -19,6 +19,7 @@ import com.excy.excy.utilities.WorkoutUtilities;
 
 public class WorkoutActivity extends AppCompatActivity {
     int workoutResId;
+    int[] powerZoneArr = {0};
 
     private static int progressStartingWidth;
 
@@ -62,8 +63,7 @@ public class WorkoutActivity extends AppCompatActivity {
 
         workoutResId = getIntent().getIntExtra(WorkoutUtilities.WORKOUT_DATA_RES_ID, 0);
 
-        setWorkoutImage();
-        setWorkoutPowerZoneImage();
+        setWorkoutImages();
 
         // Button layout
         Button pauseBtn = (Button) findViewById(R.id.btnPause);
@@ -102,86 +102,44 @@ public class WorkoutActivity extends AppCompatActivity {
         });
     }
 
-    private void setWorkoutImage() {
+    private void setWorkoutImages() {
         if (workoutResId > 0) {
             ImageView workoutImage = (ImageView) findViewById(R.id.ivWorkoutImage);
-
-            switch (workoutResId) {
-                case R.id.ibArmCandy:
-                    workoutImage.setImageResource(R.drawable.wb_arm_candy);
-                    break;
-                case R.id.ibSuperCycleCardio:
-                    workoutImage.setImageResource(R.drawable.wb_super_cycle_cardio);
-                    break;
-                case R.id.ibCycleLegBlast:
-                    workoutImage.setImageResource(R.drawable.wb_cycle_leg_blast);
-                    break;
-                case R.id.ibCoreFloorExplosion:
-                    workoutImage.setImageResource(R.drawable.wb_core_floor_explosion);
-                    break;
-                case R.id.ibArmBlast:
-                    workoutImage.setImageResource(R.drawable.wb_arm_blast);
-                    break;
-                case R.id.ibUltimateArmAndLegToning:
-                    workoutImage.setImageResource(R.drawable.wb_ultimate_arm_leg_tone);
-                    break;
-            }
-        }
-    }
-
-    private void setWorkoutPowerZoneImage() {
-        if (workoutResId > 0) {
             ImageView powerZoneImage = (ImageView) findViewById(R.id.ivZone);
 
             switch (workoutResId) {
                 case R.id.ibArmCandy:
+                    workoutImage.setImageResource(R.drawable.wb_arm_candy);
                     powerZoneImage.setImageResource(R.drawable.pz_arm_candy_graph);
-                    break;
-                case R.id.ibSuperCycleCardio:
-                    powerZoneImage.setImageResource(R.drawable.pz_super_cycle_graph);
-                    break;
-                case R.id.ibCycleLegBlast:
-                    powerZoneImage.setImageResource(R.drawable.pz_cycle_leg_blast_graph);
-                    break;
-                case R.id.ibCoreFloorExplosion:
-                    powerZoneImage.setImageResource(R.drawable.pz_core_floor_explosion_graph);
-                    break;
-                case R.id.ibArmBlast:
-                    powerZoneImage.setImageResource(R.drawable.pz_arm_blast_graph);
-                    break;
-                case R.id.ibUltimateArmAndLegToning:
-                    powerZoneImage.setImageResource(R.drawable.pz_ultimate_arm_and_leg_toning_graph);
-                    break;
-            }
-        }
-    }
-
-    private int[] getPowerZoneArray(int workoutResId) {
-        int[] powerZoneArr = {0};
-        if (workoutResId > 0) {
-            switch (workoutResId) {
-                case R.id.ibArmCandy:
                     powerZoneArr = WorkoutUtilities.PZ_ARM_CANDY_ARR;
                     break;
                 case R.id.ibSuperCycleCardio:
+                    workoutImage.setImageResource(R.drawable.wb_super_cycle_cardio);
+                    powerZoneImage.setImageResource(R.drawable.pz_super_cycle_graph);
                     powerZoneArr = WorkoutUtilities.PZ_SUPER_CYCLE_CARDIO_ARR;
                     break;
                 case R.id.ibCycleLegBlast:
+                    workoutImage.setImageResource(R.drawable.wb_cycle_leg_blast);
+                    powerZoneImage.setImageResource(R.drawable.pz_cycle_leg_blast_graph);
                     powerZoneArr = WorkoutUtilities.PZ_CYCLE_LEG_BLAST_ARR;
                     break;
                 case R.id.ibCoreFloorExplosion:
+                    workoutImage.setImageResource(R.drawable.wb_core_floor_explosion);
+                    powerZoneImage.setImageResource(R.drawable.pz_core_floor_explosion_graph);
                     powerZoneArr = WorkoutUtilities.PZ_CORE_FLOOR_EXPLOSION_ARR;
                     break;
                 case R.id.ibArmBlast:
+                    workoutImage.setImageResource(R.drawable.wb_arm_blast);
+                    powerZoneImage.setImageResource(R.drawable.pz_arm_blast_graph);
                     powerZoneArr = WorkoutUtilities.PZ_ARM_BLAST_ARR;
                     break;
                 case R.id.ibUltimateArmAndLegToning:
+                    workoutImage.setImageResource(R.drawable.wb_ultimate_arm_leg_tone);
+                    powerZoneImage.setImageResource(R.drawable.pz_ultimate_arm_and_leg_toning_graph);
                     powerZoneArr = WorkoutUtilities.PZ_ULTIMATE_ARM_LEG_TONING_ARR;
                     break;
             }
         }
-
-        return powerZoneArr;
     }
 
     public static void updateTime(int newMinutes, int newSeconds) {
