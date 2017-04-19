45b237ca182de80732b086c999d9e1925b9c0cfc
Erin Kelley <erinmkelley7@gmail.com>
2017-04-19 18:16:27 -0400

    setup dialog to send user email to reset password

--- app/src/main/java/com/excy/excy/activities/LoginActivity.java
+++ app/src/main/java/com/excy/excy/activities/LoginActivity.java
@@ -1,22 +1,65 @@
 package com.excy.excy.activities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.excy.excy.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static final String AUTH_TAG = "AUTH TAG";
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(AUTH_TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    Intent intent = new Intent(getBaseContext(), PlayActivity.class);
+                    startActivity(intent);
+                } else {
+                    // User is signed out
+                    Log.d(AUTH_TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        final EditText loginEmailET = (EditText) findViewById(R.id.etLoginEmail);
+        final EditText loginPasswordET = (EditText) findViewById(R.id.etLoginPassword);
 
         Button loginBtn =(Button) findViewById(R.id.btnLogin);
 
@@ -25,8 +68,90 @@ public class LoginActivity extends AppCompatActivity {
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), PlayActivity.class);
-                startActivity(intent);
+                String email = loginEmailET.getText().toString();
+                String password = loginPasswordET.getText().toString();
+
+                if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+                    mAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    Log.d(AUTH_TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                                    // If sign in fails, display a message to the user. If sign in succeeds
+                                    // the auth state listener will be notified and logic to handle the
+                                    // signed in user can be handled in the listener.
+                                    if (!task.isSuccessful()) {
+                                        Log.w(AUTH_TAG, "signInWithEmail:failed", task.getException());
+                                        Toast.makeText(LoginActivity.this, R.string.auth_failed,
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+
+                                }
+                            })
+                            .addOnFailureListener(LoginActivity.this, new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(LoginActivity.this, R.string.login_failed,
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            });
+                } else {
+                    Toast.makeText(LoginActivity.this, R.string.please_enter_email_password,
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        TextView forgotPasswordTV = (TextView) findViewById(R.id.tvForgotPassword);
+        forgotPasswordTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText input = new EditText(getBaseContext());
+
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT);
+
+                input.setLayoutParams(params);
+                input.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+                input.setGravity(Gravity.CENTER);
+
+                new AlertDialog.Builder(LoginActivity.this)
+                        .setTitle(R.string.forgot_password)
+                        .setMessage(R.string.forgot_password_message)
+                        .setView(input)
+                        .setPositiveButton(R.string.reset_password, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                FirebaseAuth auth = FirebaseAuth.getInstance();
+                                String emailAddress = input.getText().toString();
+
+                                if (!TextUtils.isEmpty(emailAddress)) {
+                                    auth.sendPasswordResetEmail(emailAddress)
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    if (task.isSuccessful()) {
+                                                        Log.d(AUTH_TAG, "Email sent.");
+                                                    }
+                                                }
+                                            });
+                                    dialog.dismiss();
+                                } else {
+                                    Toast.makeText(LoginActivity.this, R.string.enter_valid_email,
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .create()
+                        .show();
             }
         });
 
@@ -45,5 +170,19 @@ public class LoginActivity extends AppCompatActivity {
         /* Set up excy link view  */
         TextView excyLinkTV = (TextView) findViewById(R.id.tvLink);
         excyLinkTV.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
