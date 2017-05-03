ba1dd5ff73578d6d28a401a79795f369fd073f23
Erin Kelley <erinmkelley7@gmail.com>
2017-05-03 09:18:52 -0400

    Added a workout even when the user doesn’t submit a survey.

--- app/src/main/java/com/excy/excy/dialogs/TrackResultsDialog.java
+++ app/src/main/java/com/excy/excy/dialogs/TrackResultsDialog.java
@@ -6,13 +6,20 @@ import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.excy.excy.R;
 import com.excy.excy.activities.QuickStatsActivity;
 import com.excy.excy.activities.SurveyActivity;
+import com.excy.excy.utilities.AppUtilities;
 import com.excy.excy.utilities.WorkoutUtilities;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
+
+import static com.excy.excy.activities.SurveyActivity.SURVEY_TAG;
 
 /**
  * Created by erin.kelley on 4/11/17.
@@ -50,6 +57,7 @@ public class TrackResultsDialog extends DialogFragment {
                 })
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
+                        sendData(workout);
                         dismiss();
                         getActivity().finish();
                         Intent intent = new Intent(getActivity(), QuickStatsActivity.class);
@@ -60,5 +68,24 @@ public class TrackResultsDialog extends DialogFragment {
                 });
 
         return builder.create();
+    }
+
+    private void sendData(HashMap<String, Object> workout) {
+        String userId = workout.get("uid").toString();
+        String enjoyment = "good";
+        String location = "at home";
+        workout.put("enjoyment", enjoyment);
+        workout.put("location", location);
+        DatabaseReference mDataBaseReference = FirebaseDatabase.getInstance().getReference();
+        mDataBaseReference.child(AppUtilities.TABLE_NAME_WORKOUTS).child(userId).push()
+                .setValue(workout, new DatabaseReference.CompletionListener() {
+                    @Override
+                    public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                        if (databaseError != null) {
+                            Log.d(SURVEY_TAG, "Database Error message: " + databaseError.getMessage());
+                            Log.d(SURVEY_TAG, "Database Error details : " + databaseError.getDetails());
+                        }
+                    }
+                });
     }
 }
