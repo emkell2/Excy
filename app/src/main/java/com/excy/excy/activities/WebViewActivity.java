b19fa171f3226e6cc74c07095a02debcd041b305
Erin Kelley <erinmkelley7@gmail.com>
2017-10-18 11:57:58 -0400

    Created new learn exercises screen and web view activity.

---
+++ app/src/main/java/com/excy/excy/activities/WebViewActivity.java
@@ -0,0 +1,152 @@
+package com.excy.excy.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.excy.excy.R;
+import com.excy.excy.utilities.AppUtilities;
+import com.excy.excy.utilities.Constants;
+
+public class WebViewActivity extends AppCompatActivity {
+    public static final String KEY_YOUTUBE = "youtube.com";
+
+    private boolean mShouldPause;
+    WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        AppUtilities.setBottomNavBarIconActive(this, R.id.action_more);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottomNavigationView);
+
+        AppUtilities.removeShiftMode(bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent intent;
+                switch (item.getItemId()) {
+                    case R.id.action_play:
+                        intent = new Intent(getBaseContext(), PlayActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                        startActivity(intent);
+                        break;
+                    case R.id.action_workouts:
+                        intent = WorkoutActivity.sisActive
+                                ? new Intent(getBaseContext(), WorkoutActivity.class)
+                                : new Intent(getBaseContext(), WorkoutListActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                        startActivity(intent);
+                        break;
+                    case R.id.action_me:
+                        intent = new Intent(getBaseContext(), MeActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                        startActivity(intent);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        Intent intent = getIntent();
+        final String url = intent.getStringExtra(Constants.WEBVIEW_URL);
+
+        mWebView = (WebView) findViewById(R.id.webView);
+        mWebView.setWebViewClient(new MyWebViewClient());
+        mWebView.setWebChromeClient(new WebChromeClient());
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setPluginState(WebSettings.PluginState.ON);
+        mWebView.getSettings().setBuiltInZoomControls(true);
+        mWebView.getSettings().setSupportZoom(true);
+
+        mWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        mWebView.setScrollbarFadingEnabled(false);
+
+        mWebView.loadUrl(url);
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+
+            return true;
+        }
+
+        @Override
+        public void onLoadResource(WebView view, String url) {
+            super.onLoadResource(view, url);
+            if (isYoutube(url)) {
+                if (mWebView != null) {
+                    mWebView.onResume();
+                }
+                mShouldPause = true;
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        AppUtilities.setBottomNavBarIconActive(this, R.id.action_more);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mWebView != null && mShouldPause) {
+            mWebView.onPause();
+        }
+        mShouldPause = false;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mWebView != null) {
+            //Added so that any audio being played stops when you leave the webview
+            mWebView.destroy();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && (mWebView != null && mWebView.canGoBack())) {
+            mWebView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+
+    /**
+     * Check to see if a url is for a youtube video.
+     * @param url url to check
+     * @return true if url is youtube
+     */
+    public  boolean isYoutube(String url) {
+        boolean result = false;
+        if (url != null) {
+            result = url.contains(KEY_YOUTUBE);
+        }
+        return result;
+    }
+
+}
